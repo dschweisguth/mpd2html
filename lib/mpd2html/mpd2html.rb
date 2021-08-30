ebc4cbc40baf38731674f3bfabc377639a3a4b37
Dave Schweisguth <dave@schweisguth.org>
2021-08-30 05:56:54 -0700

    Move sorting and HTML generation to Page

--- lib/mpd2html/mpd2html.rb
+++ lib/mpd2html/mpd2html.rb
@@ -1,4 +1,3 @@
-require 'erubis'
 require 'fileutils'
 require_relative 'options'
 require_relative 'page'
@@ -12,44 +11,9 @@ module MPD2HTML
       Logger.verbose = options.verbose
       items = Parser.new.items options.files
       Page::ALL.each do |page|
-        write_html page, sort(page, items), options.output_dir
+        page.render items, options.output_dir
       end
       FileUtils.cp Dir.glob("#{File.dirname __FILE__}/../../assets/*"), options.output_dir
     end
-
-    private
-
-    def sort(page, items)
-      items.sort_by { |item| item.sort_key page.primary_sort_attribute }
-    end
-
-    def write_html(page, items, output_dir)
-      FileUtils.mkdir_p output_dir
-      IO.write "#{output_dir}/#{page.basename}.html", html(page, items)
-    end
-
-    def html(page, items)
-      template = IO.read File.expand_path("#{File.dirname __FILE__}/../../template/template.html.erb")
-      Erubis::Eruby.new(template).evaluate TemplateContext.new(page, items)
-    end
-
-    class TemplateContext
-      attr_reader :page, :items
-
-      def initialize(page, items)
-        @page = page
-        @items = items
-      end
-
-      def column_header(text, page_for_which_column_is_sorted)
-        if page == page_for_which_column_is_sorted
-          "#{text} ▽"
-        else
-          "<a href=#{page_for_which_column_is_sorted.basename}.html>#{text}</a>"
-        end
-      end
-
-    end
-
   end
 end
