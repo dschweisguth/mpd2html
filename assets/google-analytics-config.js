157987d85191b2124ceacb2aa6ca17a8836fc981
Dave Schweisguth <dave@schweisguth.org>
2022-12-31 08:34:14 -0800

    Update to current main site design. Remove Google Analytics.

--- assets/google-analytics-config.js
+++
@@ -1,7 +0,0 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'G-3MNS5PY9VX', {
-    cookie_domain: 'mpdsf.github.io',
-    cookie_flags: 'SameSite=None;Secure'
-});
